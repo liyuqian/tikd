39b906561b2c09bd845a990eb271481ab6e549c3
Yuqian Li <liyuqian79@gmail.com>
2023-12-21 20:46:10 -0800

    Move karl picture into example

--- example/karl.dart
+++ example/karl.dart
@@ -1,12 +1,124 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
+import 'package:tikd/geometry.dart';
 
 import 'package:tikd/picture.dart';
+import 'package:tikd/style.dart';
 import 'package:tikd/wrapper.dart';
 
+TikzPicture buildPicture() {
+  final picture = TikzPicture(options: [Scale(3), LineCap.round()]);
+
+  final axesStyle = CustomStyle(picture, 'axes', []);
+  final importantLineStyle = CustomStyle(
+    picture,
+    'important line',
+    [Thickness.veryThick()],
+  );
+  final informationTextStyle = CustomStyle(
+    picture,
+    'information text',
+    [Corners.round(), Fill(Color.red % 10), InnerSep(1, unit: 'ex')],
+  );
+
+  final darkGreen = Color.green % 50 + Color.black;
+  final angleColor = CustomColor(picture, 'anglecolor', darkGreen);
+  final sinColor = CustomColor(picture, 'sincolor', Color.red);
+  final cosColor = CustomColor(picture, 'coscolor', Color.blue);
+
+  final grid = XY(-1.4, -1.4) >> GridTo(XY(1.4, 1.4), step: 0.5);
+  picture.draw(grid, options: [helpLinesStyle]);
+
+  picture.draw(XY(0, 0) >> Circle(1));
+
+  final xAxisCoordinate = Coordinate('x axis');
+  final yAxisCoordinate = Coordinate('y axis');
+  final xAxis = XY(-1.5, 0) >>> XY(1.5, 0)
+    ..endNode = Node(place: Placement.right(), content: r'$x$')
+    ..coordinate = xAxisCoordinate;
+  final yAxis = XY(0, -1.5) >>> XY(0, 1.5)
+    ..endNode = Node(place: Placement.above(), content: r'$y$')
+    ..coordinate = yAxisCoordinate;
+  final axesScope = Scope(options: [axesStyle])
+    ..draw(xAxis, options: [singleArrowStyle])
+    ..draw(yAxis, options: [singleArrowStyle]);
+  picture.addScope(axesScope);
+  for (final x in <double>[-1, -.5, 1]) {
+    final tick = XY(0, 1, unit: 'pt') >>> XY(0, -1, unit: 'pt')
+      ..endNode = Node(
+          place: Placement.below(),
+          options: [Fill(Color.white)],
+          content: '\$$x\$');
+    axesScope.draw(tick, options: [Shift(XY(x, 0, unit: 'cm'))]);
+  }
+  for (final y in <double>[-1, -.5, .5, 1]) {
+    final tick = XY(1, 0, unit: 'pt') >>> XY(-1, 0, unit: 'pt')
+      ..endNode = Node(
+          place: Placement.left(),
+          options: [Fill(Color.white)],
+          content: '\$$y\$');
+    axesScope.draw(tick, options: [Shift(XY(0, y, unit: 'cm'))]);
+  }
+
+  final arc = Arc(r: 3, unit: 'mm', startAngle: 0, endAngle: 30);
+  picture.filldraw(XY(0, 0) >>> XY(3, 0, unit: 'mm') >> arc,
+      options: [Fill(Color.green % 20), Draw(angleColor)]);
+
+  picture.draw(Path(XY.polar(15, 2, unit: 'mm'))
+    ..endNode = Node(content: r'$\alpha$', options: [angleColor]));
+
+  picture.draw(
+      XY.polar(30, 1) >>> (XY.polar(30, 1) >= xAxisCoordinate)
+        ..midNode = Node(
+            content: r'$\sin \alpha$',
+            place: Placement.left(by: 1, unit: 'pt'),
+            options: [Fill(Color.white)]),
+      options: [importantLineStyle, sinColor]);
+
+  picture.draw(
+      (XY.polar(30, 1) >= xAxisCoordinate) >>> XY(0, 0)
+        ..midNode = Node(
+            content: r'$\cos \alpha$',
+            place: Placement.below(by: 1, unit: 'pt'),
+            options: [Fill(Color.white)]),
+      options: [importantLineStyle, cosColor]);
+
+  const String kUpwardLine = 'upward line';
+  const String kSlopedLine = 'sloped line';
+  picture.namePath(XY(1, 0) >>> XY(1, 1), kUpwardLine);
+  picture.namePath(XY(0, 0) >>> XY.polar(30, 1.5), kSlopedLine);
+  final intersection = Intersection(kUpwardLine, kSlopedLine, 't');
+  picture.draw(
+      XY(1, 0) >>> intersection.position
+        ..midNode = Node(
+            content: r'$\displaystyle \tan \alpha \color{black}='
+                r'\frac{{\color{red}\sin \alpha}}{\color{blue}\cos \alpha}$',
+            place: Placement.right(by: 1, unit: 'pt'),
+            options: [Fill(Color.white)]),
+      options: [intersection, Thickness.veryThick(), Color.orange]);
+
+  picture.draw(XY(0, 0) >>> intersection.position);
+
+  picture.draw(
+      Node(
+          place: Placement.right(),
+          options: [TextWidth(6, unit: 'cm'), informationTextStyle],
+          content:
+              r'The {\color{anglecolor} angle $\alpha$} is $30^\circ$ in the '
+              r'example ($\pi/6$ in radians). The {\color{sincolor}sine of '
+              r'$\alpha$}, which is the height of the red line, is'
+              r'\['
+              r'\sin \alpha = 1/2.'
+              r'\]'
+              r'By the Theorem of Pythagoras ...'),
+      options: [Shift(XY(1.85, 0))]);
+
+  return picture;
+}
+
 void main() {
-  final picture = TikzPicture();
+  final picture = buildPicture();
   final svgPath = p.join(p.dirname(Platform.script.toFilePath()), 'karl.svg');
   LatexWrapper.fromPicture(picture).makeSvg(svgPath);
 }
